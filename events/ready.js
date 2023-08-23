f5ddff12922f03cb33f0519dd98cf084a75070df
violetweather <ambersuwu@gmail.com>
2023-08-23 00:09:37 -0500

    2023.8.23

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,24 @@
 const { Events } = require('discord.js');
+const mongoose = require('mongoose');
+const mongodbURL = process.env.MONGODB_URL;
+const logger = require('../logger');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(yukong) {
+	async execute(yukong) {
 		console.log(`${yukong.user.tag} is ready.`);
+
+		if (mongodbURL) {
+			await mongoose.connect(mongodbURL || '', {
+				keepAlive: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true
+			})
+		}
+
+		if (mongoose.connect) {
+			logger.info("Database is running.")
+		}
 	},
 };
